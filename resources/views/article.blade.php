6861bcf820835696ad37125d0948ebbd86d4b2de
shk-github <Pindur.Kurz@seznam.cz>
2022-11-04 10:30:16 +0100

    vytvoření articleSection/article.blade.php

---
+++ resources/views/article.blade.php
@@ -0,0 +1,35 @@
+<article class="article">
+    <a href="#" title="Epic project" class="article__imgWrap">
+        <img src="../images/img_blog_1.jpg" alt="Epic project" loading="lazy" class="article__imgWrap__img">
+    </a>
+    <div class="article__textWrap">
+        <h2 class="article__title">
+            <a href="#" title="Epic project" class="article__link" >Epic project</a>
+        </h2>
+        <p class="article__desc">Awesome project i did in 2018. I was big experience for me</p>
+    </div>
+</article>
+
+<article class="article">
+    <a href="#" title="Anthore work i did" class="article__imgWrap">
+        <img src="../images/img_blog_2.jpg" alt="Anthore work i did" loading="lazy" class="article__imgWrap__img">
+    </a>
+    <div class="article__textWrap">
+        <h2 class="article__title">
+            <a href="#" title="Anthore work i did" class="article__link" >Anthore work i did</a>
+        </h2>
+        <p class="article__desc">Awesome project i did in 2018. I was big experience for me</p>
+    </div>
+</article>
+
+<article class="article">
+    <a href="#" title="Rebrand of this company" class="article__imgWrap">
+        <img src="../images/img_blog_3.jpg" alt="Rebrand of this company" loading="lazy" class="article__imgWrap__img">
+    </a>
+    <div class="article__textWrap">
+        <h2 class="article__title">
+            <a href="#" title="Rebrand of this company" class="article__link" >Rebrand of this company</a>
+        </h2>
+        <p class="article__desc">Awesome project i did in 2018. I was big experience for me</p>
+    </div>
+</article>
---
+++ resources/views/articlesSection.blade.php
@@ -0,0 +1,7 @@
+<section class="articlesSection">
+    <h2 class="articlesSection__title">What i did</h2>
+
+    <div class="articlesSection__wrapper">
+        @include('article')
+    </div>
+</section>
