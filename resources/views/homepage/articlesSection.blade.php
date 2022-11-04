eca7583499e5e50f87f376d9a84099ad4d00d001
shk-github <Pindur.Kurz@seznam.cz>
2022-11-04 10:48:42 +0100

    editing architecture resources/view/ for homepage

--- resources/views/homepage/articlesSection.blade.php
+++ resources/views/homepage/articlesSection.blade.php
@@ -2,6 +2,6 @@
     <h2 class="articlesSection__title">What i did</h2>
 
     <div class="articlesSection__wrapper">
-        @include('article')
+        @include('homepage.article')
     </div>
 </section>
