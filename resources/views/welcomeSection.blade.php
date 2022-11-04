e969cd071e26e71c819e00d0625d92a40b5bf496
shk-github <Pindur.Kurz@seznam.cz>
2022-11-04 10:28:26 +0100

    creating welcomeSection.blade.php

---
+++ resources/views/welcomeSection.blade.php
@@ -0,0 +1,12 @@
+<section class="welcomeSection">
+    <div class="welcomeSection__textWrap">
+        <h1 class="welcomeSection__title">Jack Jackson</h1>
+        <p class="welcomeSection__desc">What a bounder joseph stalin groomed sportacus testosterone trophy freestyle derek griffiths robot moustache, derek griffiths groomed testosterone trophy marquess of queensbury freestyle joseph stalin robot moustache Nostrilis tickler sportacus what a bounder groomed?</p>
+        <a href="#" title="Learn more" class="welcomeSection__button">Learn more</a>
+    </div>
+    <picture class="welcomeSection__imgWrap">
+        <source srcset="../images/img_girl.webp" type="image/webp">
+        <source srcset="../images/img_girl.jpg" type="image/jpeg"> 
+        <img src="../images/img_girl.jpg" alt="Lorem ipsum" loading="lazy" class="welcomeSection__mainImg">
+    </picture>
+</section>
