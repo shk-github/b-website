dac0f917a5360f399289bb0cb575a5131ae87fed
shk-github <Pindur.Kurz@seznam.cz>
2022-11-04 10:18:25 +0100

    vytvoření footer/header/mainMenu.blade.php souborů a napojení do master šablony

---
+++ resources/views/mainMenu.blade.php
@@ -0,0 +1,16 @@
+<nav class="mainMenu">
+    <div class="container">
+        <span class="mainMenu__burgerBtn js-burgerButton">Menu</span>
+        <ul class="mainMenu__group" id="mainMenuNav">
+            <li class="mainMenu__item">
+                <a href="#" class="mainMenu__item__link">Home</a>
+            </li>
+            <li class="mainMenu__item">
+                <a href="#" class="mainMenu__item__link">About me</a>
+            </li>
+            <li class="mainMenu__item">
+                <a href="#" class="mainMenu__item__link">My work</a>
+            </li>
+        </ul>
+    </div>
+</nav>
