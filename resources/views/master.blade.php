49bf687182d0de39c3d7179fa864ac354a603138
shk-github <Pindur.Kurz@seznam.cz>
2022-11-07 10:26:14 +0100

    adding favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -23,6 +23,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="keywords" content="{{config('app.name')}}, Lorem ipsum, Commodi harum necessitatibus, vero quae tempora at quia facilis">
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a7c721">
+        <meta name="apple-mobile-web-app-title" content="domain.cz">
+        <meta name="application-name" content="domain.cz">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
+
         <!-- jQuery -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer" rel="preconnect"></script>
 
